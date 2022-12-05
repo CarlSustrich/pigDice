3f1b5e03da0fdd7ba3720ff1eae7768ae1041aa4
Carl Sustrich <carl.sustrich@gmail.com>
2022-12-05 14:54:04 -0800

    working 2 player game, minus game ends at 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+//need to add a way to end game at 100 pts
+//need to display whose turn it currently is
+
+//need to add/hide 
+
+
 function Game() {
   this.players = [];
   this.activePlayer = 0;
@@ -6,6 +12,8 @@ function Game() {
 function Player() {
   this.turnScore = 0;
   this.totalScore = 0;
+  this.currentRoll = 0;
+  this.rolledYet = false;
 }
 
 function diceRoll() {
@@ -18,16 +26,20 @@ Game.prototype.switchPlayer = function () {
   } else {
     this.activePlayer = 0;
   }
-}
+};
 
 Player.prototype.rollDice = function() {
+  this.rolledYet = true;
   let roll = diceRoll();
   if (roll === 1) {
     this.turnScore = 0;
     newGame.switchPlayer();
+    this.currentRoll = roll;
+    this.rolledYet = false;
     return roll;
   } else {
     this.turnScore += roll;
+    this.currentRoll = roll;
     return roll;
   }
 };
@@ -35,9 +47,10 @@ Player.prototype.rollDice = function() {
 Player.prototype.endTurn = function() {
   this.totalScore += this.turnScore;
   this.turnScore = 0;
+  this.rolledYet = false;
   newGame.switchPlayer();
   return this.totalScore;
-}
+};
 
 let newGame = new Game();
 let player1 = new Player(0, 0);
@@ -52,55 +65,61 @@ newGame.players.push(player1, player2);
 function displayScores() {
   document.querySelector("span#player1TotalScore").innerText = player1.totalScore;
   document.querySelector("span#player2TotalScore").innerText = player2.totalScore;
-  //document.querySelector("span#player1CurrentRoll").innerText = player1.currentRoll;
-  //document.querySelector("span#player2CurrentRoll").innerText = player2.currentRoll;
-  //document.querySelector("span#player1TurnScore").innerText = player1.turnScore;
-  //document.querySelector("span#player2TurnScore").innerText = player2.turnScore;
+  document.querySelector("span#player1CurrentRoll").innerText = player1.currentRoll;
+  document.querySelector("span#player2CurrentRoll").innerText = player2.currentRoll;
+  document.querySelector("span#player1TurnScore").innerText = player1.turnScore;
+  document.querySelector("span#player2TurnScore").innerText = player2.turnScore;
+}
+
+function manageUI () {
+  let p1Controls = document.getElementById("player1Controls")
+  let p2Controls = document.getElementById("player2Controls")
+
+  if (newGame.activePlayer === 0 ) {
+    p1Controls.removeAttribute("class", "hidden");
+    p2Controls.setAttribute("class", "hidden");
+  } else {
+    p1Controls.setAttribute("class", "hidden");
+    p2Controls.removeAttribute("class", "hidden");
+  }
+
+  if (newGame.players[newGame.activePlayer].rolledYet === false) {
+      document.getElementById(`passDicePlayer${(newGame.activePlayer)+1}`).setAttribute("class", "hidden");
+  }
 }
  
 window.addEventListener("load", function() {
-  let rollBtn = document.getElementById("rollDice");
-  let passBtn = document.getElementById("passDice");
-  rollBtn.addEventListener('click', function () {
+  let player1RollBtn = document.getElementById("rollDicePlayer1");
+  let player1PassBtn = document.getElementById("passDicePlayer1");
+  let player2RollBtn = document.getElementById("rollDicePlayer2");
+  let player2PassBtn = document.getElementById("passDicePlayer2");
+
+  player1RollBtn.addEventListener('click', function () {
     newGame.players[newGame.activePlayer].rollDice();
+    document.getElementById("passDicePlayer1").removeAttribute("class", "hidden");
+    displayScores();
+    manageUI();
   });
-  rollBtn.addEventListener('click', displayScores);
-  passBtn.addEventListener('click', function () {
+
+  player1PassBtn.addEventListener('click', function () {
     newGame.players[newGame.activePlayer].endTurn();
+    document.getElementById("passDicePlayer1").setAttribute("class", "hidden");
+    displayScores();
+    manageUI();
   });
-  // passBtn.addEventListener('click', newGame.players[newGame.activePlayer].endTurn());
-  passBtn.addEventListener('click', displayScores);
+
+
+  player2RollBtn.addEventListener('click', function () {
+    newGame.players[newGame.activePlayer].rollDice();
+    document.getElementById("passDicePlayer2").removeAttribute("class", "hidden");
+    displayScores();
+    manageUI();
+  });
+
+  player2PassBtn.addEventListener('click', function () {
+    newGame.players[newGame.activePlayer].endTurn();
+    document.getElementById("passDicePlayer2").setAttribute("class", "hidden");
+    displayScores();
+    manageUI();
+  });
 });
-
-
-
-
-/* const player = {
-  currentChoice: null
-}
-const computer = {
-  currentChoice: null
-}
-const choices = ["Rock", "Paper", "Scissors"];
-
-player.currentChoice = choices[2];
-var rockBtn = document.getElementById('rock');
-var paprBtn = document.getElementById('paper');
-var scirBtn = document.getElementById('scissors');
-
-if(rockBtn || paprBtn || scirBtn) {
-    rockBtn.addEventListener('click', updateVar);
-    paprBtn.addEventListener('click', updateVar);
-    scirBtn.addEventListener('click', updateVar);
-}
-function updateVar(event){
-    if(event.target.id == 'rock'){
-        player.currentChoice = choices[0];
-    } else if (event.target.id == 'paper'){
-        player.currentChoice = choices[1];
-    } else if (event.target.id == 'scissors'){
-        player.currentChoice = choices[2];
-    }
-    console.log(player.currentChoice);
-}
-*/
